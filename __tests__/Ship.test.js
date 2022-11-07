a8618629893c663343ce0da72e3831ac4da207f8
CarlaSlattery <cslatterydev@gmail.com>
2022-11-07 11:58:03 +0000

    isolated and refactored ship tests

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -10,8 +10,18 @@ describe("Ship", () => {
     let calais;
     let itinerary;
     beforeEach(() => {
-      dover = new Port("Dover");
-      calais = new Port("Calais");
+      dover = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+        name: "Dover",
+        ships: [],
+      };
+      calais = {
+        addShip: jest.fn(),
+        removeShip: jest.fn(),
+        name: "Calais",
+        ships: [],
+      };
       itinerary = new Itinerary([dover, calais]);
       ship = new Ship(itinerary);
     });
@@ -25,33 +35,23 @@ describe("Ship", () => {
       ship.setSail();
 
       expect(ship.currentPort).toBeFalsy();
-      expect(dover.ships).not.toContain(ship);
+      expect(dover.removeShip).toHaveBeenCalledWith(ship);
     });
     it("gets added to port on instantiation", () => {
-      expect(dover.ships).toContain(ship);
+      expect(dover.addShip).toHaveBeenCalledWith(ship);
     });
-  });
+    it("is able to dock at different ports", () => {
+      ship.setSail();
+      ship.dock();
 
-  it("is able to dock at different ports", () => {
-    const dover = new Port("Dover");
-    const calais = new Port("Calais");
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship(itinerary);
-    ship.setSail();
-    ship.dock();
+      expect(ship.currentPort).toBe(calais);
+      expect(calais.addShip).toHaveBeenCalledWith(ship);
+    });
+    it("can't sail further than its itinerary", () => {
+      ship.setSail();
+      ship.dock();
 
-    expect(ship.currentPort).toBe(calais);
-    expect(calais.ships).toContain(ship);
-  });
-  it("can't sail further than its itinerary", () => {
-    const dover = new Port("Dover");
-    const calais = new Port("Calais");
-    const itinerary = new Itinerary([dover, calais]);
-    const ship = new Ship(itinerary);
-
-    ship.setSail();
-    ship.dock();
-
-    expect(() => ship.setSail()).toThrowError("End of itinerary reached");
+      expect(() => ship.setSail()).toThrowError("End of itinerary reached");
+    });
   });
 });
