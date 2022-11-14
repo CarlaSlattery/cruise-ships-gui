12e92d30fe784c7622065398df6bb88c3c1daa84
CarlaSlattery <cslatterydev@gmail.com>
2022-11-14 10:49:24 +0000

    fixed setSail method issue

--- src/controller.js
+++ src/controller.js
@@ -3,6 +3,10 @@
     constructor(ship) {
       this.ship = ship;
       this.initialiseSea();
+
+      document.querySelector("#sailbutton").addEventListener("click", () => {
+        this.setSail();
+      });
     }
     initialiseSea() {
       const backgrounds = ["./images/water0.png", "./images/water1.png"];
@@ -40,6 +44,30 @@
       const shipElement = document.querySelector("#ship");
       shipElement.style.top = `${portsElement.offsetTop + 32}px`;
       shipElement.style.left = `${portsElement.offsetLeft - 32}px`;
+    }
+    setSail() {
+      const ship = this.ship;
+      //   console.log(ship.itinerary);
+      //   console.log(ship.currentPort);
+      //  console.log(ship);
+      const currentPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+      const nextPortIndex = currentPortIndex + 1;
+      const nextPortElement = document.querySelector(
+        `[data-port-index='${nextPortIndex}']`
+      );
+      const shipElement = document.querySelector("#ship");
+      const sailInterval = setInterval(() => {
+        const shipLeft = parseInt(shipElement.style.left, 10);
+        if (shipLeft === nextPortElement.offsetLeft - 32) {
+          ship.setSail();
+          ship.dock();
+          clearInterval(sailInterval);
+        }
+        shipElement.style.left = `${shipLeft + 1}px`;
+      }, 20);
+      if (!nextPortIndex) {
+        return alert("End of the line!");
+      }
     }
   }
   if (typeof module !== "undefined" && module.exports) {
